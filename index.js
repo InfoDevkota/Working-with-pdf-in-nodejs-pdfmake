6f62be8413ea8af0ab6bc58de395d751af54fc27
Sagar Devkota <info@devkotasagar.com.np>
2021-12-27 20:59:01 +0545

    work with header and footer

--- index.js
+++ index.js
@@ -24,6 +24,81 @@ let docDefination = {
 
 let pdfDoc;
 
-pdfDoc = pdfmake.createPdfKitDocument(docDefination, {});
-pdfDoc.pipe(fs.createWriteStream('pdfs/test.pdf'));
+// pdfDoc = pdfmake.createPdfKitDocument(docDefination, {});
+// pdfDoc.pipe(fs.createWriteStream('pdfs/test.pdf'));
+// pdfDoc.end();
+
+
+
+let content = [{
+    text: "Hello World",
+    alignment: 'center',
+    fontSize: 25
+}]
+
+for (let i = 0; i < 50; i++) {
+    content.push({
+        text: `${i}) a random test.. `
+    })
+}
+
+let headerfooterDoc = {
+    header: {
+        margin: [0, 20, 0, 0],
+        alignment: 'center',
+
+        image: 'images/info.png',
+        height: 100
+
+    },
+
+    //
+    // static footer
+    // footer: {
+    //     margin: [72, 0, 72, 0],
+    //     fontSize: 10,
+    //     columns: [{
+    //             with: 'auto',
+    //             alignment: 'left',
+    //             text: '© Blogger Nepal 2022'
+    //         }
+
+    //     ],
+    // },
+    //
+    // for dynamic footer
+
+    footer: (currentPage, pageCount, pageSize) => {
+        return {
+            margin: [72, 0, 72, 0],
+            fontSize: 10,
+            columns: [{
+                    with: 'auto',
+                    alignment: 'left',
+                    text: '© Blogger Nepal 2022'
+                },
+                {
+                    with: 'auto',
+                    alignment: 'right',
+                    // text: 'Page | 1'
+                    text: [{
+                            color: '#7f7f7f',
+                            text: 'Page | '
+                        },
+                        {
+                            text: currentPage
+                        }
+                    ]
+                }
+
+            ],
+        }
+    },
+
+    content: content,
+    pageMargins: [72, 120, 72, 50],
+}
+
+pdfDoc = pdfmake.createPdfKitDocument(headerfooterDoc, {});
+pdfDoc.pipe(fs.createWriteStream('pdfs/headerfooter.pdf'));
 pdfDoc.end();
